a444c0a8f48fb1cf44e6d481296608ba82f9ed61
정우현 <wjddngus102@naver.com>
2021-10-24 16:39:55 +0900

    강의평가 틀짜기 성공 별점주기만 하면됨

--- app/src/main/java/com/example/comit/LectureEval/LectureEvalActivity.java
+++ app/src/main/java/com/example/comit/LectureEval/LectureEvalActivity.java
@@ -5,20 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
 
 import com.example.comit.ChatbotActivity;
 import com.example.comit.R;
+import com.example.comit.databinding.CommunityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+
+
 public class LectureEvalActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab2;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lectureeval_main);
+        CommunityMainBinding binding = CommunityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fab2 = binding.fab2;
 
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -29,6 +43,27 @@ public class LectureEvalActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         //상단바 제거
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController.setGraph(R.navigation.nav_graph_java_2);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.MainFragment2) {
+                    fab2.setVisibility(View.INVISIBLE);
+                    fab2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            navController.navigate(R.id.action_MainFragment_to_NewPostFragment_2);
+                        }
+                    });
+                } else {
+                    fab2.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        //onbackpressed를넣어서 오류처리해야될까
 
 
         Intent intent = new Intent(this, ChatbotActivity.class);
@@ -42,5 +77,14 @@ public class LectureEvalActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (navController.getCurrentDestination().getId() == R.id.MainFragment2) {
+            super.onBackPressed();
+        } else {
+            navController.navigate(R.id.MainFragment2);
+        }
     }
 }
